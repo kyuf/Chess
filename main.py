12309484c3fc1b62b911bff5a5a864cedb6f2bac
kyuf <jeff.jl.wen@gmail.com>
2016-01-24 00:33:23 -0800

    added player initialization

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
 Main file
 '''
 import board
+import player
 import os
 
 #create initial state of board
 os.system('clear')
 space = board.initiate()
 board.generate(space)
+
+#initialize players
+white = player.Player('w')
+black = player.Player('b')
