bce06284fa0154508c3e838bd0c1ee503c20c367
kyuf <jeff.jl.wen@gmail.com>
2016-01-24 18:09:27 -0800

    setup skeleton for move logic. moved clear to board.py

--- main.py
+++ main.py
@@ -2,14 +2,35 @@
 Main file
 '''
 import board
-import player
-import os
+import player  
 
 #create initial state of board
-os.system('clear')
 space = board.initiate()
 board.generate(space)
 
-#initialize players
+#initialize players and turn counter
 white = player.Player('w')
 black = player.Player('b')
+
+#turn = 0 for white and turn = 1 for black
+turn = 0
+players = {0: white, 1: black}
+
+#loop for player move inputs
+while True:
+    notation = input('%s to play: ' % players[turn])
+    
+    #check if move is legal
+    try:
+        players[turn].move(notation, space)
+        #set to next player's turn
+        turn = 1 - turn
+        
+        #update board
+        board.generate(space)
+    
+    #declare re-input for move
+    except:
+        board.generate(space)
+        print('Illegal move')
+    
