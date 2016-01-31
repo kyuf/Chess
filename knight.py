288c74d6d2d2e1377e4d32c6078b128927b478b6
kyuf <jeff.jl.wen@gmail.com>
2016-01-30 21:14:09 -0800

    added code to move

--- knight.py
+++ knight.py
@@ -6,8 +6,13 @@ from piece import Piece
 class Knight(Piece):
     note = 'N'
     
-    def move(self, spaces, notation):
+    def move(self, spaces, notation, newSpace):
         #use spaces and notation to determine if move is legal
-        #return oldSpace, newSpace, capture
-        #capture is either type of piece captured or None 
-        pass
+        #return oldSpace
+        rO, fO, rN, fN = self.partitionSpaces(newSpace)
+        if (fO == fN or rO == rN
+                or abs(ord(fO)-ord(fN)) + abs(ord(rO)-ord(rN)) != 3):
+            return None
+        else:
+            return self.space
+        
