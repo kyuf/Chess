0a317890f528421755c9b856bb372652eb7860aa
kyuf <jeff.jl.wen@gmail.com>
2016-01-31 00:50:59 -0800

    added code to move rook

--- rook.py
+++ rook.py
@@ -10,8 +10,24 @@ class Rook(Piece):
     def disableCastle(self):
         self.canCastle = False
     
-    def move(self, spaces, notation):
+    def move(self, spaces, notation, newSpace):
         #use spaces and notation to determine if move is legal
-        #return oldSpace, newSpace, capture
-        #capture is either type of piece captured or None 
-        pass
+        #return oldSpace
+        rO, fO, rN, fN = self.partitionSpaces(newSpace)
+        #moving vertically
+        if fO == fN:
+            dy = ord(rN) - ord(rO)
+            #check if spaces in between are empty
+            for i in range(1, abs(dy)):
+                if spaces[fO+chr(ord(rO)+dy//abs(dy)*i)] != '  ':
+                    return None
+        #moving horizontally
+        elif rO == rN:
+            dx = ord(fN) - ord(fO)
+            #check if spaces in between are empty
+            for i in range(1, abs(dx)):
+                if spaces[chr(ord(fO)+dx//abs(dx)*i)+rO] != '  ':
+                    return None
+        else:
+            return None
+        return self.space
