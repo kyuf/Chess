5d88968f9892b1033ffcd94f9fa455e82fb56c41
kyuf <jeff.jl.wen@gmail.com>
2016-04-18 22:33:14 -0700

    completed en passant and promotion movements

--- pawn.py
+++ pawn.py
@@ -2,53 +2,59 @@
 Pawn subclass
 '''
 from piece import Piece
-from knight import *
-from bishop import *
-from rook import *
-from queen import *
 
 class Pawn(Piece):
     #modify __init__ to determine forawrd variable based off Piece color
     def __init__(self, color, space, note):
         Piece.__init__(self, color, space, note)
-        self.hasNotMoved = True
+        self.vulnerable = False
         self.forward = 1 if self.color == 'w' else -1
     
-    '''
-    def move(self, spaces, notation, newSpace, capturedPiece, player):
-        oldSpace = notation[0] + str(int(newSpace[1])-player.forward)
-        #check if player has pawn on oldSpace
-        if oldSpace in player.pieces['P']:
-            #check if capturing files are adjacent
-            if (capturedPiece
-                    and abs(ord(newSpace[0])-ord(oldSpace[0])) != 1):
-                raise RuntimeError('Capturing pawn files must be adjacent')
-        #check if pawn is moving double
-        #must be on starting rank and have clear path
-        elif not capturedPiece:
-            if (notation[0] + player.ranks[1] in player.pieces['P']
-                    and spaces[oldSpace] == '  '
-                    and notation[-1] == player.ranks[2]):
-                oldSpace = notation[0] + player.ranks[1]
-                #pawn is vulnerable
-                player.vulnerableSpace = newSpace
-            else:
-                raise RuntimeError('Pawn move illegal')
-    '''
+    def move(self, spaces, notation, newSpace):
+        #check if pawn is in valid column
+        if self.space[0] != notation[0]:
+            return None
 
-    def promote(self, notation, newSpace):
-        #method to promote pawn to new piece
-        #promote to knight
-        if notation[-1] == 'N':
-            return Knight(self.color, newSpace)
-        #promote to knight
-        elif notation[-1] == 'B':
-            return Bishop(self.color, newSpace)
-        #promote to knight
-        elif notation[-1] == 'R':
-            return Rook(self.color, newSpace)
-        #promote to knight
-        elif notation[-1] == 'Q':
-            return Queen(self.color, newSpace)
+        #remove any existing vulnerability
+        if self.vulnerable:
+            self.vulnerable = False
+
+        #use spaces and notation to determine if move is legal
+        rO, fO, rN, fN = self.partitionSpaces(newSpace)
+        dy = ord(rN) - ord(rO)
+
+        #no capture is being made
+        if not 'x' in notation:
+            #single space move
+            if dy == self.forward:
+                return self.space
+            #double space move
+            elif dy == self.forward * 2:
+                #check that pawn has not moved
+                if rO != '2' and rO != '7':
+                    return None
+                #check if middle space is empty
+                mid = spaces[fO+chr(ord(rO)+self.forward)]
+                if mid == '  ':
+                    self.vulnerable = True
+                    return self.space 
+                else:
+                    return None
+        #capture is being made
+        else:
+            #capture must be single diagonal
+            if abs(ord(fO)-ord(fN)) == 1 and dy == self.forward:
+                return self.space
+            else:
+                return None
+
+    #state of vulnerability
+    def isVul(self):
+        return self.vulnerable
+
+    #return space where vulnerable pawn would be captured and pawn space
+    def getVulSpace(self):
+        if self.vulnerable:
+            return self.space[0] + chr(ord(self.space[1]) + self.forward)
         else:
             return None
--- player.py
+++ player.py
@@ -4,19 +4,21 @@ made.
 '''
 import castle
 import king
+from knight import *
+from bishop import *
+from rook import *
+from queen import *
 
 class Player:
     #pieces contains dictionary with sets of current positions for each type
     #of piece
     def __init__(self, color):
         self.color = color
-        #color used to determine starting ranks and forward pawn direction
+        #color used to determine starting ranks
         if self.color == 'w':
-            self.ranks = '124'
-            self.forward = 1
+            self.ranks = '128'
         else:
-            self.ranks = '875'
-            self.forward = -1
+            self.ranks = '871'
         #add initial pawn positions
         self.pieces = {}
         self.pieces['P'] = set()
@@ -29,16 +31,14 @@ class Player:
         self.pieces['B'] = {'c'+r, 'f'+r}
         self.pieces['Q'] = {'d'+r}
         self.pieces['K'] = {'e'+r}
+        #no pawns vulnerable at start of game
+        self.vulSpaces = None
     
     #moves follow PGN notation
     def move(self, notation, spaces, opponent):
         #vulnerable pawns can be captured en passant
         #clear any previously vulnerable pawns
-        
-        #
-        #FIX THIS!!!
-        #
-        self.vulnerableSpace = None
+        self.vulSpaces = None
         
         #remember current spaces in case move prevented by check
         tmpSpaces = spaces
@@ -56,24 +56,28 @@ class Player:
             #pawn promotion will put '=X' at end of notation where X is new
             #piece type
             newSpace = notation[-4:-2]
-            promoting = True
+            if newSpace[1] == self.ranks[2]:
+                promoting = True
+            else:
+                raise RuntimeError('Can only promote on last rank')
         #check that new space is within the board
         if newSpace[0] not in 'abcdefgh' or newSpace[1] not in '12345678':
             raise RuntimeError('Cannot move outside of board')
         #check for capture
         capturedPiece = spaces[newSpace] if 'x' in notation else None
         
-        #cannot capture king or piece of same color
+        #check if capture is legal
         if capturedPiece:
             #captured space cannot be empty unless en passant
             if capturedPiece == '  ':
-                if (opponent.vulnerableSpace == 
-                        newSpace[0] + str(int(newSpace[1])-self.forward)):
-                    capturedPiece = spaces[opponent.vulnerableSpace]
+                if opponent.vulSpaces and opponent.vulSpaces[0] == newSpace:
+                    capturedPiece = spaces[opponent.vulSpaces[1]]
                 else:
                     raise RuntimeError('No piece to capture')
+            #cannot capture king
             elif type(capturedPiece) == king.King:
                 raise RuntimeError('Cannot capture King')
+            #cannot capture piece of same color
             elif capturedPiece.color == self.color:
                 raise RuntimeError('Cannot capture own piece')
             
@@ -85,43 +89,46 @@ class Player:
         #determine piece being moved
         #pawn if first letter is lowercase
         if notation[0] in 'abcdefgh':
-            pieceType = 'P'
-            #pawns move differently for white and black
-            oldSpace = notation[0] + str(int(newSpace[1])-self.forward)
-            #check if player has pawn on oldSpace
-            if oldSpace in self.pieces['P']:
-                #check if capturing files are adjacent
-                if (capturedPiece
-                        and abs(ord(newSpace[0])-ord(oldSpace[0])) != 1):
-                    raise RuntimeError('Capturing pawn files must be adjacent')
-            #check if pawn is moving double
-            #must be on starting rank and have clear path
-            elif not capturedPiece:
-                if (notation[0] + self.ranks[1] in self.pieces['P']
-                        and spaces[oldSpace] == '  '
-                        and notation[-1] == self.ranks[2]):
-                    oldSpace = notation[0] + self.ranks[1]
-                    #pawn is vulnerable
-                    self.vulnerableSpace = newSpace
-                else:
-                    raise RuntimeError('Pawn move illegal')
-        
+            pieceType = 'P'      
         #rook, knight, bishop, queen, king if first letter is R, N, B, Q, K
         elif notation[0] in 'RNBQK':
-            pieceType = notation[0]
-            #try move with all player pieces
-            for pieceSpace in self.pieces[pieceType]:
-                oldSpace = spaces[pieceSpace].move(spaces, notation, newSpace)
-                if oldSpace:
-                    break
-            if not oldSpace:
-                #move is illegal
-                raise RuntimeError('Move found to be illegal')
-        
+            pieceType = notation[0]      
         #no piece found
         else:
             raise RuntimeError('Incorrect piece notation')
         
+        #find correct piece to move
+        for pieceSpace in self.pieces[pieceType]:
+            oldSpace = spaces[pieceSpace].move(spaces, notation, newSpace)
+            if oldSpace:
+                break
+        if not oldSpace:
+            #move is illegal
+            raise RuntimeError('Move found to be illegal')
+
+        #special considerations for moving pawns
+        if pieceType == 'P':
+            #check if pawn was made vulnerable
+            if spaces[oldSpace].isVul():
+                self.vulSpaces = spaces[oldSpace].getVulSpace(), newSpace
+            #check if pawn is being promoted
+            elif promoting:
+                newPiece = notation[-1]
+                if newPiece in 'RNBQ':
+                    self.pieces['P'].remove(oldSpace)
+                    self.pieces[newPiece].add(oldSpace)
+                    if newPiece == 'R':
+                        spaces[oldSpace] = Rook(self.color, oldSpace, 'R')
+                    elif newPiece == 'N':
+                        spaces[oldSpace] = Knight(self.color, oldSpace, 'N')
+                    elif newPiece == 'B':
+                        spaces[oldSpace] = Bishop(self.color, oldSpace, 'B')
+                    else:
+                        spaces[oldSpace] = Queen(self.color, oldSpace, 'Q')
+                    pieceType = newPiece
+                else:
+                    raise RuntimeError('Invalid promotion')
+
         #remove captured piece from opponent's pieces
         if capturedPiece:
             spaces[capturedPiece.space] = '  '
@@ -135,7 +142,6 @@ class Player:
         self.pieces[pieceType].remove(oldSpace)
         self.pieces[pieceType].add(newSpace)
         #return updated spaces and captured piece type
-        print(self.vulnerableSpace)
         return spaces, opponent
         
     
